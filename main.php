82497b8a91112d9f87f8442a56ba63dfcb328b9e
samyside <samyside@yandex.ru>
2025-01-31 17:00:11 +0300

    В main.php функция getShapeCornersCount() для запуска и тестирования

--- main.php
+++ main.php
@@ -1,36 +1,47 @@
 <?php
 
-class ShapeException extends Exception {
-  public function getMessage(): string {
-    return "Something goes wrong about Shape class";
-  }
-}
+require_once 'vendor/autoload.php';
 
-interface ShapeInterface {
-  public function getCornersCount(string ...$shapes): string;
-}
+use Samyside\Shapes\Square;
+use Samyside\Shapes\Circle;
+use Samyside\Exceptions\ShapeNotFoundException;
 
-abstract class Shape implements ShapeInterface {
-  public function getCornersCount(string ...$shapes): string {
-    $result = "";
-    foreach ($shapes as $shape) {
-      $shapeClassname = ucfirst(strtolower($shape));
-      try {
-        $o = new $shapeClassname();
-        $cornerCounts = $o->getCornerCounts();
-        $result .= "{$shape} - {$cornerCounts}\n";
-      } catch (ShapeException $e) {
-        $result .= "{$shape} - 0\n";
+// Определяем цвета
+$red      = "\033[31m";
+$green    = "\033[32m";
+$yellow   = "\033[33m";
+$blue     = "\033[34m";
+$magenta  = "\033[35m";
+$cyan     = "\033[36m";
+$color    = "\033[0m"; // Сброс цвета
+
+/**
+ * Тестирование
+ */
+
+echo "{$green}Start...{$color}\n";
+
+/**
+ * 
+ */
+function getShapeCornersCount(string ...$shapesName): string {
+  $result = '';
+  foreach ($shapesName as $shapeName) {
+    try {
+      $classname = "Samyside\\Shapes\\" . ucfirst(strtolower($shapeName));
+      if (!class_exists($classname)) {
+        throw new ShapeNotFoundException("Shape is not defined", 1);
       }
+      $shape = new $classname();
+      $corners = $shape->getCornersCount();
+      $result .= "{$shapeName} - {$corners}\n";
+    } catch (ShapeNotFoundException $e) {
+      $result .= "{$shapeName} - not defined\n";
     }
-    return $result;
   }
+  return $result;
 }
 
-class Square extends Shape {
+echo getShapeCornersCount('square', 'circle', 'triangle', 'ASDFqwer');
 
-}
-
-class Circle extends Shape {
-
-}
+echo "{$red}Finish.{$color}\n";
