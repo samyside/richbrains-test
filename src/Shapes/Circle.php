32302966585987d13b5e9dfbc2b15e86ea9e1790
Vlad Babenko <samyside@yandex.ru>
2025-02-05 15:32:32 +0300

    Убраны лишние переопределения методов getCornersCount()

--- src/Shapes/Circle.php
+++ src/Shapes/Circle.php
@@ -3,10 +3,5 @@
 namespace Samyside\Shapes;
 
 class Circle extends Shape {
-  #[\Override]
   protected int $corners = 0;
-
-  public function getCornersCount(): int {
-    return $this->corners;
-  }
 }
